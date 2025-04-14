215524fc73d53860148031c30994ff21e6283be1
Markovic Boban <sayby.19@gmail.com>
2025-04-14 23:50:43 +0200

    homepage/roster/notfound

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,52 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul className="bg-teal-200 text-2xl text-blue-950 flex flex-row justify-evenly p-3">
-        <li>
-          <NavLink to=" ">Home</NavLink>
+    <nav className="sticky top-0 z-50 bg-white shadow-md p-5">
+      <ul className="text-2xl text-[#FFFF00] flex items-center justify-between px-6">
+        {/* Leva strana */}
+        <li
+          className="text-2xl font-bold text-[#ffff00]"
+          style={{
+            textShadow:
+              "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+          }}
+        >
+          Hello, user
         </li>
-        <li>
-          <NavLink to=" "> Leaderboard</NavLink>
+
+        {/* Logo u sredini */}
+        <li className="absolute left-1/2 transform -translate-x-1/2">
+          <NavLink to=" ">
+            <img
+              className="w-40"
+              src="/src/assets/poke-logo.png"
+              alt="poke-logo"
+            />
+          </NavLink>
+        </li>
+
+        {/* Desna strana */}
+        <li className="flex gap-6">
+          <NavLink
+            className="text-2xl font-bold text-[#ffff00]"
+            style={{
+              textShadow:
+                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            }}
+            to="leaderboard"
+          >
+            Leaderboard
+          </NavLink>
+          <NavLink
+            className="text-2xl font-bold text-[#ffff00]"
+            style={{
+              textShadow:
+                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            }}
+            to="roster"
+          >
+            Roster
+          </NavLink>
         </li>
       </ul>
     </nav>
