5ac72d5309009a32fd27a8b108a705e8a34a24a3
Markovic Boban <sayby.19@gmail.com>
2025-04-15 12:01:21 +0200

    frontend 03

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import backgroundImage from "../assets/background.jpg";
 import pokeBall from "../assets/pokeball4.gif";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
+  const [input, setInput] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
-  const goToRoster = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("User", input);
+    setInput("");
     navigate("/roster");
   };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+    if (value.length > 2 && value.length < 20) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
   return (
-    <div className="flex flex-col h-screen bg-gray-100">
-      {/* Hero Section */}
-      <div
-        className="flex flex-col items-center justify-center h-full w-full bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-        }}
-      >
-        {/* <h1
-          className="text-2xl font-bold text-[#ffff00]"
+    <>
+      <div className="flex flex-col h-screen bg-gray-100 top-0">
+        <div
+          className="flex flex-col items-center justify-center h-full w-full bg-cover bg-center"
           style={{
-            textShadow:
-              "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            backgroundImage: `url(${backgroundImage})`,
           }}
         >
-          Pokemon Battle Game
-        </h1> */}
-        <p
-          className="text-2xl text-center font-bold text-[#ffff00]"
-          style={{
-            textShadow:
-              "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
-          }}
-        >
-          🎮 Pick your team of 3 Pokémons from the roster <br />
-          and head into battle – 3 vs 3!
-        </p>
-        <img
-          onClick={goToRoster}
-          src={pokeBall}
-          alt="Pikachu"
-          className="w-64 h-auto rounded-xl shadow-lg cursor-pointer"
-        />
-        {/* <div className="mt-6 space-x-4">
-          <button
-            onClick={goToRoster}
-            className="px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300"
+          <p
+            className="text-2xl text-center font-bold text-[#ffff00]"
+            style={{
+              textShadow:
+                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            }}
           >
-            3vs3 Battle
-          </button>
-        </div> */}
+            🎮 Pick your team of 3 Pokémons from the roster <br />
+            and head into battle – 3 vs 3!
+          </p>
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              onKeyDown={(e) => {
+                e.key === "Enter" && e.preventDefault();
+              }}
+              type="text"
+              name="user"
+              placeholder="Your name"
+              className="py-4 px-2 bg-white text-black"
+            />
+
+            <button type="submit" className={`${isValid ? "block" : "hidden"}`}>
+              <img
+                src={pokeBall}
+                alt="Pikachu"
+                className="w-64 h-auto rounded-xl shadow-lg cursor-pointer"
+              />
+            </button>
+          </form>
+        </div>
+        <footer className="p-1 text-center bg-gray-800 text-gray-400">
+          &copy; 2025 Pokemon Battle Game. All rights reserved. <br /> Patrick
+          || Ramil || Boban
+        </footer>
       </div>
-      {/* Footer */}
-      <footer className="p-1 text-center bg-gray-800 text-gray-400">
-        &copy; 2025 Pokemon Battle Game. All rights reserved. <br /> Patrick ||
-        Ramil || Boban
-      </footer>
-    </div>
+    </>
   );
 }
 
